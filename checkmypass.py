d42c47772c019cbf4f2cfab922f09ee43925e15e
Fabrizio De Cicco <fabri.deci@gmail.com>
2023-04-30 16:49:20 +0000

    refactor: add timeout to get request

--- checkmypass.py
+++ checkmypass.py
@@ -4,7 +4,7 @@ import sys
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
-    res = requests.get(url)
+    res = requests.get(url, timeout=60)
     if res.status_code != 200:
         raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
     return res
